49eb88c28c356df13404f287d43b6be378981879
siporoz <smart.cpa@mail.ru>
2019-05-10 10:19:10 +0500

    Initial Commit

---
+++ WaterControl/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WaterControl
+//
+//  Created by Владимир on 10.05.2019.
+//  Copyright © 2019 Siporoz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
